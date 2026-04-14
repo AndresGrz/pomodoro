6c7b9a5084d8e6e64bebeb75f96ab7c818a3c101
Andres Garza <andresgarzadavila1996@gmail.com>
2026-04-14 14:07:57 -0600

    Se mejoro el diseno de la app, se agregaron botones para cambiar el tiempo, pero no tienen funcionalidad todavia

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ import pygame
 def main(page: ft.Page):
     pygame.mixer.init()
     page.title = "Pomodoro"
-    page.window_width = 300
-    page.window_height = 400
     page.padding = 0
     page.spacing = 0
     page.fonts = {
         "Roboto": "assets/fonts/Roboto-Regular.ttf",
+        "Audiowide": "assets/fonts/Audiowide-Regular.ttf",
+        "Oi": "assets/fonts/Oi-Regular.ttf",
+        "Quantico": "assets/fonts/Quantico-Regular.ttf"
     }
 
 
     # Inicialización de variables para el temporizador
-    WORK_TIME = 5 #25 * 60
-    BREAK_TIME = 5 #5 * 60
-    TEXT_STYLE = ft.TextStyle(font_family="Roboto")
+    WORK_TIME = 10 # 25 * 60
+    BREAK_TIME = 10 # 5 * 60
     is_running = False
     is_work = True
     remaining_time = WORK_TIME
@@ -48,19 +48,19 @@ def main(page: ft.Page):
     def format_time(seconds):
         mins = seconds // 60
         secs = seconds % 60
-        return f"{mins:02}:{secs:02}"
+        return f"{mins:02}:{secs:02}".replace("", " ")[1:-1]
 
 
     # Función para actualizar la interfaz con el tiempo restante y el estatus actual
     def update_ui():
-        timer_text.value = format_time(remaining_time)
+        timer_text.content.value = format_time(remaining_time)
 
         if is_work:
-            status_text.value = "WORK"
+            status_text.content.value = "WORK"
             bg.bgcolor = "#112D4E"
 
         else:
-            status_text.value = "BREAK"
+            status_text.content.value = "BREAK"
             bg.bgcolor = "#3F72AF"
 
         page.update()
@@ -129,47 +129,121 @@ def main(page: ft.Page):
 
 
     # Textos
-    timer_text = ft.Text(
-        value="25:00",
-        size=60,
-        color="#F9F7F7",
-        font_family="Roboto",
+    timer_text = ft.Container(
+        content=ft.Text(
+            size=90,
+            color="#F9F7F7",
+            font_family="Roboto",
+        ),
+        padding=ft.Padding.symmetric(vertical=-20)
     )
 
 
-    status_text = ft.Text(
-        value="WORK",
-        size=40,
-        color="#F9F7F7",
-        font_family="Roboto",
+    status_text = ft.Container(
+        content=ft.Text(
+            value="WORK",
+            size=60,
+            color="#F9F7F7",
+            font_family="Oi",  
+        ),
+        padding=ft.Padding.symmetric(vertical=20)
     )
-
+    
 
     # Botones
     control_btn = ft.Button(
         content=ft.Text(
             "Start",
             weight=ft.FontWeight.BOLD,
-            size=16,
+            size=30,
         ),
         on_click=timer,
         style=ft.ButtonStyle(
             bgcolor = "#DBE2EF",   #button color
             color = "#3F72AF",      #text color
-            text_style=TEXT_STYLE,
-            padding=20
+            text_style=ft.TextStyle(font_family="Quantico"),
+            padding=ft.Padding.symmetric(horizontal=40, vertical=17)
         )
     )
     
 
     reset_btn = ft.IconButton(
         icon=ft.Icons.RESTART_ALT_OUTLINED,
+        icon_size=42,
         on_click=reset_timer,
         style=ft.ButtonStyle(
             bgcolor = "#DBE2EF",   #button color
             color = "#3F72AF"  ,    #text color
-            text_style=TEXT_STYLE
+            padding=9
         )
+    )
+
+
+    up_btn_minutes = ft.IconButton(
+        icon=ft.Icons.KEYBOARD_ARROW_UP_ROUNDED,
+        icon_size=16,
+        padding=-5,
+        style=ft.ButtonStyle(            
+            color = "#DBE2EF"  ,    #text color
+        )
+    )
+
+
+    down_btn_minutes = ft.IconButton(
+        icon=ft.Icons.KEYBOARD_ARROW_DOWN_ROUNDED,
+        icon_size=16,
+        padding=-5,
+        style=ft.ButtonStyle(
+            color = "#DBE2EF"  ,    #text color
+        )
+    )
+
+
+    up_btn_seconds = ft.IconButton(
+        icon=ft.Icons.KEYBOARD_ARROW_UP_ROUNDED,
+        icon_size=16,
+        padding=-5,
+        style=ft.ButtonStyle(            
+            color = "#DBE2EF"  ,    #text color
+        )
+    )
+
+
+    down_btn_seconds = ft.IconButton(
+        icon=ft.Icons.KEYBOARD_ARROW_DOWN_ROUNDED,
+        icon_size=16,
+        padding=-5,
+        style=ft.ButtonStyle(
+            color = "#DBE2EF"  ,    #text color
+        )
+    )
+
+
+    timer_controls = ft.Column(
+        [
+            ft.Row(
+                [
+                    up_btn_minutes,
+                    up_btn_seconds
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                spacing=150
+            ),
+            ft.Container(
+                timer_text,
+                padding=ft.Padding.symmetric(vertical=0)
+            ),
+            ft.Row(
+                [
+                    down_btn_minutes,
+                    down_btn_seconds
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                spacing=150
+            )
+        ],
+        alignment=ft.MainAxisAlignment.START,
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER
     )
 
 
@@ -181,14 +255,18 @@ def main(page: ft.Page):
         margin=0,
         content = ft.Column([
                 status_text,
-                timer_text,
-                ft.Row(
-                    [control_btn, reset_btn],
-                    alignment=ft.MainAxisAlignment.CENTER
+                timer_controls,
+                ft.Container(
+                    ft.Row(
+                        [control_btn, reset_btn],
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        spacing=40
+                    ),
+                    margin=ft.Margin.only(top=30)
                 )
             ],
             alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
         )
     )
 
